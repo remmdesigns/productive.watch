a01be76a1d1de36b1b6fe796ae43accaebb5c31a
AND.EVV <andrew60199@gmail.com>
2023-05-23 21:00:01 +1000

    progressed on the stopwatch and timers functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Stopwatch from './components/Stopwatch'
+import Timer from './components/Timer'
+import History from './components/History'
+import Footer from './components/Footer'
 
 function App() {
   // https://www.geeksforgeeks.org/create-a-stop-watch-using-reactjs/
   // https://stackoverflow.com/questions/8043026/how-to-format-numbers-by-prepending-0-to-single-digit-numbers
 
   const start = 0 //(3600*10-10)
-  const pomodoro = 5
+  const pomodoro = 10
 
   const [stopwatch, setStopWatch] = useState(start)
   const [running, setRunning] = useState(false)
+
   const [timer, setTimer] = useState(pomodoro)
   const [pomodoroCount, setPomodoroCount] = useState(0)
-  const [isOnBreak, setIsOnBreak] = useState(false)
-  const [breakMessage, setBreakMessage] = useState(false)
+  const [timerStage, setTimerStage] = useState(1)
 
   useEffect(() => {
-    if (running) {
-      // Stopwatch's total
+    if (running && timerStage < 3) {
       // console.log(stopwatch)
       // How many seconds - total remainder 60 to get the second
       // console.log(("0" + (stopwatch % 60)).slice(-2))
@@ -25,21 +29,19 @@ function App() {
       // console.log(("0" + Math.floor((stopwatch / 60) % 60)).slice(-2))
       // How many hours - Returns the greatest total less than or equal to 3600 (one hour) then use the remainder of 60 to get the hour
       // console.log(("0" + Math.floor((stopwatch / 3600) % 60)).slice(-2))
-      // Pomodoro Timer
-      // console.log(timer)
 
       const stopwatchId = setInterval(() => {
         setStopWatch(prevState => prevState + 1)
       }, 1000)    
   
       return () => {
-        clearInterval(stopwatchId)
-        
+        clearInterval(stopwatchId)        
       }
     }    
   })
 
   useEffect(() => {
+    // Could improve this. Since it doesn't make sense that running needs to be going for the timer to be. Because we need to switch on running in other parts of the code to get the timer counting down
     if (running && timer > 0) {
       // console.log(timer)
 
@@ -54,78 +56,72 @@ function App() {
   })
 
   useEffect(() => {
-      if (timer === 0 && !isOnBreak) {
+      if (timer === 0 && timerStage === 1) {
         setPomodoroCount(prevState => prevState + 1)
-        setBreakMessage(true)
+        setTimerStage(2)
       } 
   }, [timer])
 
-  const handleSetTimer = (time) => {
-    setTimer(time)
+  useEffect(() => {
+    if (timer === 0 && timerStage === 3) {
+      setTimerStage(4)
+    } 
+  }, [timer])
+
+  const handleRunning = (boolean) => {
+    if (boolean) {
+      setRunning(boolean)
+    } else{
+      setRunning(running => !running)
+    }
+  }
+
+  const handleReset = () => {
+    setRunning(false)
+    setStopWatch(start)
+    setTimer(pomodoro)
+    setPomodoroCount(0)
+    setTimerStage(1)
+
+    // Record session...
+  }
+
+  const handleTimerStage = (stage) => {
+    // 1: Working, 2: Work session has ended, 3: On break, 4: Break has ended
+
+    if(stage === 1) {
+      setTimer(10)
+    } 
+    if (stage === 3) {
+      // Logic to handle if it is a 5 or 10 min break
+      setTimer(5)
+    }
+
+    setTimerStage(stage)
   }
 
   return (
     <>
-      <header>
-        <h1>productive.watch<span>TM</span></h1>
-        <p>Since 2023</p>
-        <p><span>REMM</span>DESIGNS<span>TM</span></p>
-      </header>
+      <Header />
       <main>
-        <section>
-          <h2>Today's Stop Watch</h2>
-          <div className='card'>
-            <p className='card-text'>
-              <span>{("" + Math.floor((stopwatch / 3600) % 60)).slice(-2)}</span>:
-              <span>{("0" + Math.floor((stopwatch / 60) % 60)).slice(-2)}</span>'
-              <span>{("0" + (stopwatch % 60)).slice(-2)}</span>
-            </p>
-          </div>
-          <button onClick={() => setRunning(running => !running)}>Start / Stop</button>
-          <button onClick={() => {
-              setRunning(false)
-              setStopWatch(start)
-              setTimer(pomodoro)
-              setPomodoroCount(0)
-              setBreakMessage(false)
-              setIsOnBreak(false)
-            }
-          }>Reset</button>
-        </section>
-        <section>
-          <h2>Pomodoro Timer</h2>
-          {pomodoroCount > 0 && <h3>{pomodoroCount} session{pomodoroCount > 1 && `'s`} complete!</h3>}
-          <div className='card'>
-            <p className='card-text'>
-              <span>{("0" + Math.floor((timer / 60) % 60)).slice(-2)}</span>'
-              <span>{("0" + (timer % 60)).slice(-2)}</span>
-            </p>
-          </div>
-          {!isOnBreak && timer > 0 && <button onClick={() =>setTimer(pomodoro)}>Reset</button>}
-          {breakMessage && <>
-            <button onClick={() => {
-                setTimer(300)
-                setIsOnBreak(true)
-              }              
-            }>You've earned a 5 minute break! Would you like to start it?</button>
-            <button onClick={() =>setTimer(pomodoro)}>Cancel and start another 25 minute session</button>
-          </>}
-          {isOnBreak &&  <button>Pause break</button>}
-        </section>
-        {/* <section>
-          <h2>Last seven entries</h2>
-          <table>
-
-          </table>
-        </section> */}
+        <Stopwatch 
+          stopwatch={stopwatch} 
+          running={running}
+          timerStage={timerStage}
+          handleRunning={handleRunning} 
+          handleReset={handleReset}
+          handleTimerStage={handleTimerStage}
+        />
+        <Timer 
+          timer={timer}
+          pomodoroCount={pomodoroCount}
+          timerStage={timerStage}
+          handleTimerStage={handleTimerStage}
+          handleRunning={handleRunning} 
+        />
+        <History />
       </main>
-      {/* <footer>
-        <p>Designed and developed by</p>
-        <p><span>REMM</span>DESIGNS<span>TM</span></p>
-        <p>We are web development company and here to help you design and develop your websites and/or to design your app ideas.</p>
-        <p>Learn more about us at:</p>
-        <a href='www.remmdesigns.com' target='_blank'>www.remmdesigns.com</a>
-      </footer> */}
+      <Footer />
     </>
   )
 }
---
+++ src/components/History.jsx
@@ -0,0 +1,13 @@
+const History = () => {
+
+    return (
+        <section>
+            <h2>Last seven entries</h2>
+            <table>
+
+            </table>
+        </section> 
+    )
+}
+
+export default History
